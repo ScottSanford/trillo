354390c912a43078117dc6616495fbace7d9efd5
Scott Sanford <scottsanford1217@gmail.com>
2020-04-04 15:20:33 -0500

    extract another list

--- react-trillo/src/components/content/description/recommend/Recommend.js
+++ react-trillo/src/components/content/description/recommend/Recommend.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import './Recommend.css'
 
+const friendRecommendList = [
+  { img: 'img/user-3.jpg', name: 'Friend 1'},
+  { img: 'img/user-4.jpg', name: 'Friend 2'},
+  { img: 'img/user-5.jpg', name: 'Friend 3'},
+  { img: 'img/user-6.jpg', name: 'Friend 4'},
+]
+
 export default function Recommend() {
+
+  const recommendFriendImages = friendRecommendList.map(friend => {
+    return (
+      <img src={friend.img} alt={friend.name} className="recommend__photo" />
+    )
+  })
+
   return (
     <div className="recommend">
-      <p className="recommend__count">
-        Lucy and 3 other friends recommend this hotel.
-              </p>
+      <p className="recommend__count">Lucy and 3 other friends recommend this hotel.</p>
       <div className="recommend__friends">
-        <img src="img/user-3.jpg" alt="Friend 1" className="recommend__photo" />
-        <img src="img/user-4.jpg" alt="Friend 2" className="recommend__photo" />
-        <img src="img/user-5.jpg" alt="Friend 3" className="recommend__photo" />
-        <img src="img/user-6.jpg" alt="Friend 4" className="recommend__photo" />
+        {recommendFriendImages}
       </div>
     </div>
   )
