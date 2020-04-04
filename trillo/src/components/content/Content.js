ba830a2b17f9aa7a6cae3bced7dc5bf3900e4da7
Scott Sanford <scottsanford1217@gmail.com>
2020-04-04 13:53:26 -0500

    make an Overview componenet

--- trillo/src/components/content/Content.js
+++ trillo/src/components/content/Content.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Content.css'
 import Sidebar from '../sidebar/Sidebar'
 import Gallery from '../gallery/Gallery'
+import Overview from '../overview/Overview'
 
 export default function Content() {
   return (
@@ -10,38 +11,7 @@ export default function Content() {
 
       <main className="hotel-view">
         <Gallery />
-
-        <div className="overview">
-          <h1 className="overview__heading">Hotel Las Palmas</h1>
-          <div className="overview__stars">
-            <svg className="overview__icon-star">
-              <use xlinkHref="img/sprite.svg#icon-star" />
-            </svg>
-            <svg className="overview__icon-star">
-              <use xlinkHref="img/sprite.svg#icon-star" />
-            </svg>
-            <svg className="overview__icon-star">
-              <use xlinkHref="img/sprite.svg#icon-star" />
-            </svg>
-            <svg className="overview__icon-star">
-              <use xlinkHref="img/sprite.svg#icon-star" />
-            </svg>
-            <svg className="overview__icon-star">
-              <use xlinkHref="img/sprite.svg#icon-star" />
-            </svg>
-          </div>
-          <div className="overview__location">
-            <svg className="overview__icon-location">
-              <use xlinkHref="img/sprite.svg#icon-location-pin" />
-            </svg>
-            <button className="btn-inline">Albufeira, Portugal</button>
-          </div>
-
-          <div className="overview__rating">
-            <div className="overview__rating-average">8.6</div>
-            <div className="overview__rating-count">429 votes</div>
-          </div>
-        </div>
+        <Overview />
 
         <div className="detail">
           <div className="description">
