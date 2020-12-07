041b7375cc6078e26eb4804c9d81a7afbb703459
Scott <ssanford@thinkmarkets.com>
2020-12-06 19:03:00 -0600

    connect to the database

--- react-trillo/app.js
+++ react-trillo/app.js
@@ -1,4 +1,32 @@
 const express = require('express')
+const mysql = require('mysql')
+const dotenv = require('dotenv')
+
+dotenv.config({
+	path: './.env'
+})
+
+const {
+	DATABASE_HOST,
+	DATABASE_USER,
+	DATABASE_PASSWORD,
+	DATABASE
+} = process.env
+
+const connection = mysql.createConnection({
+	host: DATABASE_HOST,
+	user: DATABASE_USER,
+	password: DATABASE_PASSWORD,
+	database: DATABASE
+})
+
+connection.connect((err) => {
+	if (err) {
+		console.log('database err', err)
+	} else {
+		console.log('MySQL Connected...')
+	}
+})
 
 const app = express()
 
